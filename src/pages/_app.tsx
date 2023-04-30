1fe0b0487473390f9dc43cf3d503eb3964bcd242
Luke Mino-Altherr <lminoaltherr@gmail.com>
2023-04-30 10:29:15 -0400

    feat: add google anayltics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,48 @@
 import '../styles/global.css';
 
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+import { useEffect } from 'react';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+import { GA_MEASUREMENT_ID, pageview } from '@/utils/googleAnalytics';
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}', {
+            page_path: window.location.pathname,
+          });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
+};
 
 export default MyApp;
---
+++ src/utils/googleAnalytics.ts
@@ -0,0 +1,24 @@
+export const GA_MEASUREMENT_ID =
+  process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || '';
+export const pageview = (url: string) => {
+  (window as any).gtag('config', GA_MEASUREMENT_ID, {
+    page_path: url,
+  });
+};
+export const event = ({
+  action,
+  category,
+  label,
+  value,
+}: {
+  action: string;
+  category: string;
+  label: string;
+  value: number;
+}) => {
+  (window as any).gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value,
+  });
+};
